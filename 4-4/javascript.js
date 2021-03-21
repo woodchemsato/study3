7bd3b4c2414e6cd430efa223e9bdacaa8ff5fc82
hondaminori <hondaminori.ta3@gmail.com>
2021-03-21 19:52:53 +0900

    [add] 4-4完成

---
+++ 4-4/javascript.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  // ulで全部取得
+  // console.log($('ul').children('li'));
+
+  // 復習　２番目を取得
+  // console.log($('ul').children('li').eq(1));
+
+  // 要素の変更系 バリエーション
+
+  // 要素のcssを変更
+  // $('body').css('background-color','red');
+
+  // 指定したidの文字をフォントサイズ20pxに変更
+  // $('li#id4-4_3').css('font-size', '36px'); 
+
+  // classだって付与可能！
+  // $('li#id4-4_2').addClass('addClass'); 
+
+  // 該当するのが複数なら全部に付与される
+  // $('li').addClass('addClass'); 
+
+});
