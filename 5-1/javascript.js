b13e3bdf2594c5ee49b7f45c19bacbae9664156a
hondaminori <hondaminori.ta3@gmail.com>
2021-03-21 21:27:58 +0900

    [add] 5-1完成

---
+++ 5-1/javascript.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  // h1をクリックすると、文字をはまぐちと書き換える
+  // ここに記述することにより、処理ロジックをdocument.ready時に
+  // h1に仕込んでおく、というイメージ
+  $('h1').on('click', function() {
+    $('h1').text('はまぐち');
+  });
+
+  // 前に出てきたソースコードでは、ready時に実行させた
+  // 今回はボタンクリックで実行させる
+
+  // append 最後に要素を追加
+  $('#addlibtn').on('click',function() {
+    $('ul').append('<li>appendで要素を追加しました的な</li>');
+  });
+
+  // prepend 最初に要素を指定
+  $('#prependlibtn').on('click',function() {
+    $('ul').prepend('<li>pretendで要素を追加しました的な</li>');
+  });
+
+  // 課題：削除を作ってみましょう
+
+});
